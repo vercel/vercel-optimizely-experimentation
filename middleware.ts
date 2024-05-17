7dd2804e86af6790be335ca3658d79500c34e33e
Alex <alex.hawley@vercel.com>
2024-05-17 16:08:19 -0500

    chore: Refactor tracking of product purchase and middleware flags

--- middleware.ts
+++ middleware.ts
@@ -7,24 +7,29 @@ import {
 import * as flags from "@/lib/middleware-flags";
 
 export const config = {
-  matcher: ["/"],
+  matcher: ["/", "/product/:path*", "/cart", "/success"],
 };
 
 export async function middleware(request: NextRequest) {
-  const context = {
-    /* pass any context data your flag may need */
-  };
-  const flagSet = await unstable_decide(request, flags, context);
-  const code = await unstable_serialize(flagSet, flags);
+  let response = NextResponse.next();
 
-  // rewrites the request to the variant for this flag combination
-  const nextUrl = new URL(
-    `/${code}${request.nextUrl.pathname}${request.nextUrl.search}`,
-    request.url
-  );
-  const response = NextResponse.rewrite(nextUrl, { request });
+  // decide via middleware flags for the homepage only
+  if (request.nextUrl.pathname === "/") {
+    const context = {
+      /* pass any context data your flag may need */
+    };
+    const flagSet = await unstable_decide(request, flags, context);
+    const code = await unstable_serialize(flagSet, flags);
 
-  // set a shopper cookie if one doesn't exist
+    // rewrites the request to the variant for this flag combination
+    const nextUrl = new URL(
+      `/${code}${request.nextUrl.pathname}${request.nextUrl.search}`,
+      request.url
+    );
+    response = NextResponse.rewrite(nextUrl, { request });
+  }
+
+  // set a shopper cookie if one doesn't exist or has been cleared
   if (!request.cookies.has("shopper")) {
     const newShopperId = Math.random().toString(36).substring(2);
     response.cookies.set("shopper", newShopperId);
